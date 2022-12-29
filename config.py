a47d50db6cc55db1fb1cb4f5e8b98e035d3eff53
Suhail Saeed Usmani <suhailsaeedme@gmail.com>
2022-12-30 02:10:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,25 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 10248430
-    API_HASH = "42396a6ff14a569b9d59931643897d0d"
+    APP_ID = 2004115
+    API_HASH = "a2e576aa2ba6035450fdc84b407d2daf"
     # the name to display in your alive message
-    ALIVE_NAME = "rishabh"
+    ALIVE_NAME = "🄹🄰🅁🅅🄸🅂"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
+    DB_URI = "postgres://kmfmurku:3WrFn3G1nLccvMfLbYUO-0sZiJBbGsjy@berry.db.elephantsql.com/kmfmurku"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "your session"
+    STRING_SESSION = "1BVtsOKYBu6O-jE_9I0zeopYBb6QSS57AvBCwlw8yIG73LVvTGXEDub8ZBiaiam3BCUcvACC60Fq-8JP2QGavKxrurI63isUIMfVngaLadwGZpGl-XCJOhwmBnloYg1kYQcz4cGxwj-Ai3y5g1ThqWCr-LVG7p7I_XAcSo_jkwOBP8895Johcj8fzC1A80GROp2h4oaWxaiJ1yQgRwtZU89FmRvRtqCaNdVYJtql9augX4YAbSYwce_2Fc-2BO4lt1miSpSjrFOaiJXklZTUwC7nlbrLTG0Kp3Nx8jZWgOuNDoq2cKHWTksi_jEjA4V8rTDelNhfCD9Tc8wzvbJffT_upduTI5X4="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "your token"
+    TG_BOT_TOKEN = "5602311236:AAGI8ChGJL6WZSE5ljBno5qQsZ5uuoxkj-0"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID =
+    PRIVATE_GROUP_BOT_API_ID = -1001888827644
     # command handler
     COMMAND_HAND_LER = "."
-    VCMODE = "True"
+    VCMODE = "False"
     VC_SESSION = "your 2nd id telethon session" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
-    EXTERNAL_REPO = "https://github.com/rishabhanand2/tha_plugins"
+    EXTERNAL_REPO = "https://github.com/mrdevil786/Thanos-Pro.git"
     #don't edit this 
-    THANOSABUSE = "False"#don't edit this else error in 
+    THANOSABUSE = "True"#don't edit this else error in 
